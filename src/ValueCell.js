3a72352bcbdefb153167b19029cd354dd346e803
Malcolm Stone <malcolm.stone91@gmail.com>
2016-08-13 00:06:59 +0100

    make cell input load existing value

--- src/ValueCell.js
+++ src/ValueCell.js
@@ -5,52 +5,57 @@ import './Cell.css';
 import './ValueCell.css';
 
 export class ValueCell extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state ={
-        value: ''
-    }
-  }
-
-  render() {
-    const classNames = classes('Cell ValueCell', {
-      'ValueCell--selected': this.props.isSelected,
-      'ValueCell--entered': this.props.isEntered
-    });
-
-    let cellContent;
-
-    if (this.props.isEntered) {
-        cellContent = <input 
-            type="text"
-            className="ValueCell__input"
-            ref={el => el ? el.focus() : undefined} 
-            onChange={this.handleChange.bind(this)}
-            onKeyUp={this.handleKeyUp.bind(this)} 
-            onBlur={this.handleBlur.bind(this)} />
-    } else {
-        cellContent =this.props.value || UNICODE_BLANK 
+        this.state = {
+            value: ''
+        }
     }
 
-    return (
-      <div className={classNames} onClick={this.props.onClick}>
-        {cellContent}
-      </div>
-    );
-  }
+    componentWillMount() {
+        this.setState({ value: this.props.cell.value || '' })
+    }
 
-  handleChange(e) {
-      this.setState({ value: e.target.value });
-  }
+    render() {
+        const classNames = classes('Cell ValueCell', {
+            'ValueCell--selected': this.props.isSelected,
+            'ValueCell--entered': this.props.isEntered
+        });
 
-  handleKeyUp(e) {
-      if (e.key === 'Enter') {
-          this.props.onExit(this.props.cell, this.state.value);
-      }
-  }
+        let cellContent;
 
-  handleBlur(e) {
-      this.props.onExit(this.props.cell, this.state.value);
-  }
+        if (this.props.isEntered) {
+            cellContent = <input
+                type="text"
+                className="ValueCell__input"
+                value={this.state.value}
+                ref={el => el ? el.focus() : undefined}
+                onChange={this.handleChange.bind(this) }
+                onKeyUp={this.handleKeyUp.bind(this) }
+                onBlur={this.handleBlur.bind(this) } />
+        } else {
+            cellContent = this.props.value || UNICODE_BLANK
+        }
+
+        return (
+            <div className={classNames} onClick={this.props.onClick}>
+                {cellContent}
+            </div>
+        );
+    }
+
+    handleChange(e) {
+        this.setState({ value: e.target.value });
+    }
+
+    handleKeyUp(e) {
+        if (e.key === 'Enter') {
+            this.props.onExit(this.props.cell, this.state.value);
+        }
+    }
+
+    handleBlur(e) {
+        this.props.onExit(this.props.cell, this.state.value);
+    }
 }
