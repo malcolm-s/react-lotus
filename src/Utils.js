16a0123ac6b6a5e6fdfbc01b7dea19d6ad3bcc05
Malcolm Stone <malcolm.stone91@gmail.com>
2016-08-14 14:10:54 +0100

    remove unused collection methods from utils

--- src/Utils.js
+++ src/Utils.js
@@ -1,19 +1,3 @@
-// todo: move to array utils
-export function range(n) {
-  let result = [];
-
-  for (var index = 1; index <= n; index++) {
-    result.push(index);
-  }
-
-  return result;
-}
-
-// todo: move to array utils
-export function max(array) {
-  return array.reduce((prev, curr) => curr > prev ? curr : prev);
-}
-
 export function classes(constant, conditional) {
   const conditionalClasses = Object.keys(conditional)
     .filter(key => conditional[key])
@@ -27,5 +11,5 @@ export const UNICODE_BLANK = '\u00a0';
 // todo: make more generic - deep equal? or have Cell as a class with compare(other) method
 export function referenceMatch(reference1, reference2) {
   return reference1.x === reference2.x
-      && reference1.y === reference2.y
+    && reference1.y === reference2.y
 }
