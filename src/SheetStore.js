cf89cd6b2ba8f2de1777d2bbe9028191b79b8b17
Malcolm Stone <malcolm.stone91@gmail.com>
2016-12-11 23:14:05 +0000

    add CellEvaluator.js to hold evaluation logic

--- src/SheetStore.js
+++ src/SheetStore.js
@@ -1,5 +1,6 @@
 import { flatMap } from './CollectionUtils';
 import { referenceMatch, fromStringReference, asXLabel } from './CellReferenceUtils';
+import { CellEvaluator } from './CellEvaluator';
 
 const STORAGE_KEY = 'cells';
 
@@ -7,6 +8,7 @@ export class SheetStore {
     constructor(cellParser) {
         this._cellDict = JSON.parse(localStorage.getItem(STORAGE_KEY) || '{}');
         this._cellParser = cellParser;
+        this._cellEvaluator = new CellEvaluator(this);
     }
 
     get cells() {
@@ -58,56 +60,16 @@ export class SheetStore {
          */
         console.log(`${asXLabel(cell.reference.x)}${cell.reference.y}`);
         const expression = this._cellParser.parse(cell.value);
-        const evaluated = this._evaluateExpression(expression);
+        const evaluated = this._cellEvaluator.evaluate(expression);
         console.log('evaluated value', evaluated);
 
         return evaluated;
     }
 
-    _evaluateExpression(expression) {
-        const type = expression.type;
-        if (type === 'constant') {
-            return expression.value;
-        } else if (type === 'expression') {
-            let context = expression.children.reduce(this._evaluate.bind(this), { });
-            console.log('evaluated context', context);
-            if (context.fn) {
-                context.value = context.fn.apply(this, context.args);
-            }
-            console.log('evaluated context', context);
-            return context.value;
-        }
-    }
+    getDisplayValueFromStringReference(stringRef) {
+        const reference = fromStringReference(stringRef);
+        const cell = this.getCell(reference);
 
-    _evaluate(context, next) {
-        console.log('curr', context);
-        console.log('next', next);
-
-        if (next.type === 'constant') {
-            if (context.fn) {
-                context.args.push(next.value);
-            } else {
-                context.value = next.value;
-            }
-        } else if (next.type === 'reference') {
-            // cyclic references
-            const reference = fromStringReference(next.value);
-            const cell = this.getCell(reference);
-            const value = this.getDisplayValue(cell);
-
-            if (context.fn) {
-                context.args.push(value);
-            } else {
-                context.value = value;
-            }
-        } else if (next.type === 'function') {
-            context.args = [];
-            context.args.push(context.value);
-            context.fn = function (a, b) {
-                return parseFloat(a) + parseFloat(b);
-            };
-        }
-
-        return context;
+        return this.getDisplayValue(cell);
     }
 }
